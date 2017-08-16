bd54c0506ed087d199d2c4c135ec03a209218cb9
Sai Charan <saicharanrao@gmail.com>
2017-08-15 21:46:12 -0500

    Online cart initial set up done successfully.

---
+++ shoppingBackEnd/src/main/java/net/charan/shoppingBackEnd/App.java
@@ -0,0 +1,13 @@
+package net.charan.shoppingBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
