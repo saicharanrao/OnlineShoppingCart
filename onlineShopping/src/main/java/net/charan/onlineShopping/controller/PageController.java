4ebe94e3617a45898f04b448728c7a7764dfd9fb
Sai Charan <saicharanrao@gmail.com>
2017-08-16 21:57:27 -0500

    Added Dispatcher Servlet , Spring bean Configuration File

---
+++ onlineShopping/src/main/java/net/charan/onlineShopping/controller/PageController.java
@@ -0,0 +1,17 @@
+package net.charan.onlineShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = {"/","/index","/home"})
+	
+	public ModelAndView index()
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Spring MVC");
+		return mv;
+	}
+}
