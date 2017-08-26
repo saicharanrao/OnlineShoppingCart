d011425d4d4e1a66b1afe322d675f654790d85bd
Sai Charan <saicharanrao@gmail.com>
2017-08-25 20:42:20 -0500

    Home Page and added all JS,CSS,Fonts

--- onlineShopping/src/main/java/net/charan/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/net/charan/onlineShopping/controller/PageController.java
@@ -6,12 +6,37 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping(value = {"/","/index","/home"})
 	
+	
+	@RequestMapping(value = {"/","/index","/home"})
 	public ModelAndView index()
 	{
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring MVC");
 		return mv;
 	}
+	
+	/*@RequestMapping(value = "/test")
+	public ModelAndView test (@RequestParam(value = "greeting",required = false) String greeting)
+	{	
+		if(greeting == null)
+		{
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test (@PathVariable("greeting") String greeting)
+	{	
+		if(greeting == null)
+		{
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
 }
