a0c1807f99fcdd968379900b0b71bc00f1101e60
Sai Charan <saicharanrao@gmail.com>
2017-08-26 16:01:24 -0500

    Views Seperated.Menu changed.

---
+++ onlineShopping/src/main/webapp/assests/js/myapp.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+	//Code to handle active Menu
+	
+	switch(menu){
+		case 'About Us':
+			$('#about').addClass('active');
+			break;
+		case 'Contact Us':
+			$('#contact').addClass('active');
+			break;
+		default:
+			$('#home').addClass('active');
+			break;
+	}
+	
+});
