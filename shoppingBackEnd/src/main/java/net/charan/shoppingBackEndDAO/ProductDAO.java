b56effb589b44641e88e9a050284946ac7edf9f4
Sai Charan <saicharanrao@gmail.com>
2017-09-03 14:38:58 -0500

    New DB tables,Product DAO

---
+++ shoppingBackEnd/src/main/java/net/charan/shoppingBackEndDAO/ProductDAO.java
@@ -0,0 +1,21 @@
+package net.charan.shoppingBackEndDAO;
+
+import java.util.List;
+
+import net.charan.shoppingBackEndDTO.Product;
+
+public interface ProductDAO {
+	
+	Product get(int productId);
+	List<Product> list();
+	boolean add(Product product);
+	boolean update(Product product);
+	boolean delete(Product product);
+	
+	//Methods for business logic
+	List<Product> listActiveProducts();
+	List<Product> listActiveProductsByCategory(int categoryId);
+	List<Product> getLatestActiveProducts(int count);
+	
+	
+}
