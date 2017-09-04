aa767f66ef7d3f78ba55a0c6e9bc37d291109519
Sai Charan <saicharanrao@gmail.com>
2017-09-04 12:51:25 -0500

    SLF4J Logging Capability

--- onlineShopping/src/main/java/net/charan/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/net/charan/onlineShopping/controller/PageController.java
@@ -1,5 +1,8 @@
 package net.charan.onlineShopping.controller;
 
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,8 @@ import net.charan.shoppingBackEndDTO.Product;
 @Controller
 public class PageController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
@@ -25,6 +30,10 @@ public class PageController {
 	{
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home Page");
+		
+		logger.info("Inside PageController index method - INFO");
+		logger.info("Inside PageController index method - DEBUG");
+		
 		mv.addObject("categories", categoryDAO.list()); //Passing the list of categories
 		mv.addObject("userClickHome",true);
 		return mv;
